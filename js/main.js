20d2b7e8b4bebe32a77210eb70a4d90e10f31ee5
Lautaro <lautiotegui@gmail.com>
2022-08-25 23:28:46 -0300

    nuevos cambios

---
+++ js/main.js
@@ -0,0 +1,156 @@
+//  Carro
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+// Abrir Carro
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+// Cerrar Carro
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+// Carrito js
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+// Funciones
+function ready() {
+  // Remover items del carro
+  let reomveCartButtons = document.getElementsByClassName("cart-remove");
+  console.log(reomveCartButtons);
+  for (let i = 0; i < reomveCartButtons.length; i++) {
+    let button = reomveCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+  // Cambio de cantidades
+  let quantityInputs = document.getElementsByClassName("cart-quantity");
+  for (let i = 0; i < quantityInputs.length; i++) {
+    let input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+  // Añadir al carro
+  let addCart = document.getElementsByClassName("add-cart");
+  for (let i = 0; i < addCart.length; i++) {
+    let button = addCart[i];
+    button.addEventListener("click", addCartClicked);
+  }
+  // Boton Comprar para que ande
+  document
+    .getElementsByClassName("btn-buy")[0]
+    .addEventListener("click", buyButtonClicked);
+}
+// Boton Comprar
+function buyButtonClicked() {
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Tu compra fue realizada con exito!',
+    showConfirmButton: false,
+    timer: 1500
+  })
+  let cartContent = document.getElementsByClassName("cart-content")[0];
+  while (cartContent.hasChildNodes()) {
+    cartContent.removeChild(cartContent.firstChild);
+  }
+  updatetotal();
+}
+
+// Removes items del carro
+function removeCartItem(event) {
+  let buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updatetotal();
+}
+// Cambio de Cantidades
+function quantityChanged(event) {
+  let input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updatetotal();
+}
+// Añadir al Carrito
+function addCartClicked(event) {
+  let button = event.target;
+  let shopProducts = button.parentElement;
+  let title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+  let price = shopProducts.getElementsByClassName("price")[0].innerText;
+  let productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+  addProductToCart(title, price, productImg);
+  updatetotal();
+}
+function addProductToCart(title, price, productImg) {
+  let cartShopBox = document.createElement("div");
+  cartShopBox.classList.add("cart-box");
+  let cartItems = document.getElementsByClassName("cart-content")[0];
+  let cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+  for (let i = 0; i < cartItemsNames.length; i++) {
+    if (cartItemsNames[i].innerText == title) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Ya has añadido este producto al carrito!',
+      });
+      return;
+    }
+  }
+  let cartBoxContent = `
+                        <img src="${productImg}" alt="" class="cart-img">
+                        <div class="detail-box">
+                            <div class="cart-product-title">${title}</div>
+                            <div class="cart-price">${price}</div>
+                            <input type="number" value="1" class="cart-quantity">
+                        </div>
+                        <!-- Remove Cart -->
+                        <i class='bx bxs-trash-alt cart-remove' ></i>`;
+  cartShopBox.innerHTML = cartBoxContent;
+  cartItems.append(cartShopBox);
+  cartShopBox
+    .getElementsByClassName("cart-remove")[0]
+    .addEventListener("click", removeCartItem);
+  cartShopBox
+    .getElementsByClassName("cart-quantity")[0]
+    .addEventListener("change", quantityChanged);
+}
+
+// Refrescar Total
+function updatetotal() {
+  let cartContent = document.getElementsByClassName("cart-content")[0];
+  let cartBoxes = cartContent.getElementsByClassName("cart-box");
+  let total = 0;
+  for (let i = 0; i < cartBoxes.length; i++) {
+    let cartBox = cartBoxes[i];
+    let priceElement = cartBox.getElementsByClassName("cart-price")[0];
+    let quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    let price = parseFloat(priceElement.innerText.replace("$", ""));
+    let quantity = quantityElement.value;
+    total = total + price * quantity;
+  }
+  // Si el precio contiene centavos
+  total = Math.round(total * 100) / 100;
+
+  document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+}
+
+//Local Storage
+/*function addLocalStorage() {
+  localStorage.setItem('Carrito', JSON.stringify)
+  //localStorage.setItem("Carrito , precio");
+}
+window.onload = function(){
+  const storage = JSON.parse(localStorage.getItem('carrito'));
+  if(storage){
+    carrito = storage;
+    renderCarrito()
+  }
+}
+cart.push(cart);
+let cart_JSON = JSON.stringify(cart);
+
+console.log(cart);
+show_cart(cartShopBox);*/
